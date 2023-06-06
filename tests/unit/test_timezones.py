94a2faf353ef41f80cc2acbc93f63bbfed1a61ba
Eric Dodge <edodge11@gmail.com>
2023-06-06 10:46:40 -0400

    Fix timezone tests

--- tests/unit/test_timezones.py
+++ tests/unit/test_timezones.py
@@ -10,7 +10,6 @@ class TestTimezones:
 
         response = client.get("/api/timezones")
         assert response.status_code == 200
-        print(response.json)
         # Check the response
         assert "success" in response.json
         assert response.json["success"] is True
@@ -21,6 +20,8 @@ class TestTimezones:
         assert "name" in response.json["data"][0]
         assert "abbrev" in response.json["data"][0]
         assert "utc_offset" in response.json["data"][0]
-        assert response.json["data"][1]["name"] == "EST"
-        assert response.json["data"][1]["abbrev"] == "EST"
-        assert response.json["data"][1]["utc_offset"] == "-05:00"
+
+        est_list = [item for item in response.json["data"] if item["name"] == "EST"]
+        assert len(est_list) == 1
+        assert est_list[0]["abbrev"] == "EST"
+        assert est_list[0]["utc_offset"] == "-05:00"
