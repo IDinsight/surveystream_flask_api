e5aaa77407b3a6c2ffbe601397c6724fe8898816
Eric Dodge <edodge11@gmail.com>
2023-06-14 12:38:48 -0400

    Revert unit test changes and update number of gunicorn workers

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,6 +6,6 @@ echo "Starting"
 case "$1" in
   api)
 
-	gunicorn --chdir app --timeout 300 "app:create_app()" -b "0.0.0.0:5001" -k gevent -w 5
+	gunicorn --chdir app --timeout 300 "app:create_app()" -b "0.0.0.0:5001" -k gevent -w 2
 	;;
 esac
