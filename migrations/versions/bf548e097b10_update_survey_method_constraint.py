8ef14f2bd2e2fa278a367ca8b6da1d09128446af
Griffin Muteti <griffinmuteti31@gmail.com>
2023-09-08 16:32:53 +0300

    - update revision ids

--- migrations/versions/bf548e097b10_update_survey_method_constraint.py
+++ migrations/versions/bf548e097b10_update_survey_method_constraint.py
@@ -1,7 +1,7 @@
 """Update survey method constraint
 
 Revision ID: bf548e097b10
-Revises: 33b8ce72b8e9
+Revises: 
 Create Date: 2023-09-08 12:26:45.172707
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'bf548e097b10'
-down_revision = '33b8ce72b8e9'
+down_revision = None
 branch_labels = None
 depends_on = None
 
