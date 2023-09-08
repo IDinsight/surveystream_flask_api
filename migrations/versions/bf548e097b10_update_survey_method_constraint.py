3bd0f47160de7655426657e72278cedf2b194368
Eric Dodge <edodge11@gmail.com>
2023-09-08 08:27:44 -0400

    Add empty migration file

---
+++ migrations/versions/bf548e097b10_update_survey_method_constraint.py
@@ -0,0 +1,24 @@
+"""Update survey method constraint
+
+Revision ID: bf548e097b10
+Revises: 33b8ce72b8e9
+Create Date: 2023-09-08 12:26:45.172707
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bf548e097b10'
+down_revision = '33b8ce72b8e9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
