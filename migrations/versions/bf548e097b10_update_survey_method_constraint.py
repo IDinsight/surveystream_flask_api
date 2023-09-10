f802e721ef89d3f9ba150a09d2afb4266e4b13be
Eric Dodge <edodge11@gmail.com>
2023-09-10 12:22:46 -0400

    Update migration file

--- migrations/versions/bf548e097b10_update_survey_method_constraint.py
+++ migrations/versions/bf548e097b10_update_survey_method_constraint.py
@@ -10,21 +10,27 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = 'bf548e097b10'
-down_revision = 'b8b5153f25a3'
+revision = "bf548e097b10"
+down_revision = "dcfd7fd39aa5"
 branch_labels = None
 depends_on = None
 
+
 def upgrade():
     # Modify the constraint condition to include 'mixed-mode'
+    op.drop_constraint("ck_surveys_surveying_method", "surveys")
     op.create_check_constraint(
         "ck_surveys_surveying_method",
         "surveys",
-        sa.text(
-            "surveying_method IN ('phone', 'in-person', 'mixed-mode')"
-        ),
+        sa.text("surveying_method IN ('phone', 'in-person', 'mixed-mode')"),
     )
 
+
 def downgrade():
-    # Remove the constraint
+    # Revert the constraint
     op.drop_constraint("ck_surveys_surveying_method", "surveys")
+    op.create_check_constraint(
+        "ck_surveys_surveying_method",
+        "surveys",
+        sa.text("surveying_method IN ('phone', 'in-person')"),
+    )
