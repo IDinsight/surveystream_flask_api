f32f2c1a7235bf73b0d5ded6d20bd6c9da52f19b
Griffin Muteti <griffinmuteti31@gmail.com>
2024-04-29 14:09:41 +0300

    - cover targets and enumerators in survey config status tests

--- tests/unit/test_surveys.py
+++ tests/unit/test_surveys.py
@@ -1,6 +1,9 @@
+import base64
+from pathlib import Path
 import jsondiff
 import pytest
 import re
+import pandas as pd
 
 from utils import (
     create_new_survey_admin_user,
@@ -30,6 +33,7 @@ class TestSurveys:
             "planned_start_date": "2021-01-01",
             "planned_end_date": "2021-12-31",
             "state": "Draft",
+            "prime_geo_level_uid": 1,
             "config_status": "In Progress - Configuration",
         }
 
@@ -51,6 +55,7 @@ class TestSurveys:
             "planned_start_date": "2021-01-01",
             "planned_end_date": "2021-12-31",
             "state": "Draft",
+            "prime_geo_level_uid": 1,
             "config_status": "In Progress - Configuration",
         }
 
@@ -107,6 +112,297 @@ class TestSurveys:
 
         yield
 
+    @pytest.fixture
+    def create_permission(self, client, login_test_user, csrf_token):
+        """
+        Create simple permissions
+        Expect to be used while adding roles
+        """
+        data = {"name": "WRITE", "description": "Write permission"}
+        response = client.post(
+            "/api/permissions",
+            json=data,
+            content_type="application/json",
+            headers={"X-CSRF-Token": csrf_token},
+        )
+        assert response.status_code == 201
+        assert response.json["message"] == "Permission created successfully"
+
+        return {
+            "permission_uid": response.json["permission_uid"],
+            "name": response.json["name"],
+            "description": response.json["description"],
+        }
+
+    @pytest.fixture()
+    def create_roles(
+        self, client, login_test_user, csrf_token, create_surveys, create_permission
+    ):
+        """
+        Insert new roles as a setup step for the roles tests
+        """
+
+        payload = {
+            "roles": [
+                {
+                    "role_uid": None,
+                    "role_name": "Core User",
+                    "reporting_role_uid": None,
+                    "permissions": [create_permission["permission_uid"]],
+                },
+                {
+                    "role_uid": None,
+                    "role_name": "Regional Coordinator",
+                    "reporting_role_uid": 1,
+                    "permissions": [create_permission["permission_uid"]],
+                },
+            ]
+        }
+
+        response = client.put(
+            "/api/roles",
+            query_string={"survey_uid": 1},
+            json=payload,
+            content_type="application/json",
+            headers={"X-CSRF-Token": csrf_token},
+        )
+        assert response.status_code == 200
+
+        yield
+
+    @pytest.fixture()
+    def create_geo_levels(
+        self, client, login_test_user, csrf_token, create_parent_form
+    ):
+        """
+        Insert new geo levels as a setup step for the location upload tests
+        These correspond to the geo levels found in the locations test files
+        """
+
+        payload = {
+            "geo_levels": [
+                {
+                    "geo_level_uid": None,
+                    "geo_level_name": "District",
+                    "parent_geo_level_uid": None,
+                },
+                {
+                    "geo_level_uid": None,
+                    "geo_level_name": "Mandal",
+                    "parent_geo_level_uid": 1,
+                },
+                {
+                    "geo_level_uid": None,
+                    "geo_level_name": "PSU",
+                    "parent_geo_level_uid": 2,
+                },
+            ]
+        }
+
+        response = client.put(
+            "/api/locations/geo-levels",
+            query_string={"survey_uid": 1},
+            json=payload,
+            content_type="application/json",
+            headers={"X-CSRF-Token": csrf_token},
+        )
+        assert response.status_code == 200
+
+        yield
+
+    @pytest.fixture()
+    def create_locations(
+        self,
+        client,
+        login_test_user,
+        create_geo_levels,
+        csrf_token,
+    ):
+        """
+        Upload locations csv as a setup step for the enumerators upload tests
+        """
+
+        filepath = (
+            Path(__file__).resolve().parent
+            / f"data/file_uploads/sample_locations_small.csv"
+        )
+
+        # Read the locations.csv file and convert it to base64
+        with open(filepath, "rb") as f:
+            locations_csv = f.read()
+            locations_csv_encoded = base64.b64encode(locations_csv).decode("utf-8")
+
+        # Try to upload the locations csv
+        payload = {
+            "geo_level_mapping": [
+                {
+                    "geo_level_uid": 1,
+                    "location_name_column": "district_name",
+                    "location_id_column": "district_id",
+                },
+                {
+                    "geo_level_uid": 2,
+                    "location_name_column": "mandal_name",
+                    "location_id_column": "mandal_id",
+                },
+                {
+                    "geo_level_uid": 3,
+                    "location_name_column": "psu_name",
+                    "location_id_column": "psu_id",
+                },
+            ],
+            "file": locations_csv_encoded,
+        }
+
+        response = client.post(
+            "/api/locations",
+            query_string={"survey_uid": 1},
+            json=payload,
+            content_type="application/json",
+            headers={"X-CSRF-Token": csrf_token},
+        )
+
+        assert response.status_code == 200
+
+        df = pd.read_csv(filepath, dtype=str)
+        df.rename(
+            columns={
+                "district_id": "District ID",
+                "district_name": "District Name",
+                "mandal_id": "Mandal ID",
+                "mandal_name": "Mandal Name",
+                "psu_id": "PSU ID",
+                "psu_name": "PSU Name",
+            },
+            inplace=True,
+        )
+
+        expected_response = {
+            "data": {
+                "ordered_columns": [
+                    "District ID",
+                    "District Name",
+                    "Mandal ID",
+                    "Mandal Name",
+                    "PSU ID",
+                    "PSU Name",
+                ],
+                "records": df.to_dict(orient="records"),
+            },
+            "success": True,
+        }
+        # Check the response
+        response = client.get("/api/locations", query_string={"survey_uid": 1})
+
+        checkdiff = jsondiff.diff(expected_response, response.json)
+        assert checkdiff == {}
+
+    @pytest.fixture()
+    def upload_enumerators_csv(
+        self, client, login_test_user, create_locations, csrf_token
+    ):
+        """
+        Upload the enumerators csv
+        """
+
+        filepath = (
+            Path(__file__).resolve().parent
+            / f"data/file_uploads/sample_enumerators_small.csv"
+        )
+
+        # Read the enumerators.csv file and convert it to base64
+        with open(filepath, "rb") as f:
+            enumerators_csv = f.read()
+            enumerators_csv_encoded = base64.b64encode(enumerators_csv).decode("utf-8")
+
+        # Try to upload the enumerators csv
+        payload = {
+            "column_mapping": {
+                "enumerator_id": "enumerator_id1",
+                "name": "name1",
+                "email": "email1",
+                "mobile_primary": "mobile_primary1",
+                "language": "language1",
+                "home_address": "home_address1",
+                "gender": "gender1",
+                "enumerator_type": "enumerator_type1",
+                "location_id_column": "district_id1",
+                "custom_fields": [
+                    {
+                        "field_label": "Mobile (Secondary)",
+                        "column_name": "mobile_secondary1",
+                    },
+                    {
+                        "field_label": "Age",
+                        "column_name": "age1",
+                    },
+                ],
+            },
+            "file": enumerators_csv_encoded,
+            "mode": "overwrite",
+        }
+
+        response = client.post(
+            "/api/enumerators",
+            query_string={"form_uid": 1},
+            json=payload,
+            content_type="application/json",
+            headers={"X-CSRF-Token": csrf_token},
+        )
+        print(response.json)
+        assert response.status_code == 200
+
+    @pytest.fixture()
+    def upload_targets_csv(self, client, login_test_user, create_locations, csrf_token):
+        """
+        Upload the targets csv
+        """
+
+        filepath = (
+            Path(__file__).resolve().parent
+            / f"data/file_uploads/sample_targets_small.csv"
+        )
+
+        # Read the targets.csv file and convert it to base64
+        with open(filepath, "rb") as f:
+            targets_csv = f.read()
+            targets_csv_encoded = base64.b64encode(targets_csv).decode("utf-8")
+
+        # Try to upload the targets csv
+        payload = {
+            "column_mapping": {
+                "target_id": "target_id1",
+                "language": "language1",
+                "gender": "gender1",
+                "location_id_column": "psu_id1",
+                "custom_fields": [
+                    {
+                        "field_label": "Mobile no.",
+                        "column_name": "mobile_primary1",
+                    },
+                    {
+                        "field_label": "Name",
+                        "column_name": "name1",
+                    },
+                    {
+                        "field_label": "Address",
+                        "column_name": "address1",
+                    },
+                ],
+            },
+            "file": targets_csv_encoded,
+            "mode": "overwrite",
+        }
+
+        response = client.post(
+            "/api/targets",
+            query_string={"form_uid": 1},
+            json=payload,
+            content_type="application/json",
+            headers={"X-CSRF-Token": csrf_token},
+        )
+        assert response.status_code == 200
+
     def test_create_survey_for_super_admin(
         self, client, login_test_user, create_surveys, test_user_credentials
     ):
@@ -132,7 +428,7 @@ class TestSurveys:
                     "planned_start_date": "2021-01-01",
                     "planned_end_date": "2021-12-31",
                     "state": "Draft",
-                    "prime_geo_level_uid": None,
+                    "prime_geo_level_uid": 1,
                     "config_status": "In Progress - Configuration",
                     "last_updated_at": "2023-05-30 00:00:00",
                     "created_by_user_uid": test_user_credentials["user_uid"],
@@ -144,7 +440,7 @@ class TestSurveys:
                     "last_updated_at": "2024-01-25 12:38:41.277939",
                     "planned_end_date": "2021-12-31",
                     "planned_start_date": "2021-01-01",
-                    "prime_geo_level_uid": None,
+                    "prime_geo_level_uid": 1,
                     "project_name": "Test Project",
                     "state": "Draft",
                     "survey_description": "A survey admin survey",
@@ -263,7 +559,7 @@ class TestSurveys:
                     "planned_start_date": "2021-01-01",
                     "planned_end_date": "2021-12-31",
                     "state": "Draft",
-                    "prime_geo_level_uid": None,
+                    "prime_geo_level_uid": 1,
                     "config_status": "In Progress - Configuration",
                     "last_updated_at": "2023-05-30 00:00:00",
                     "created_by_user_uid": test_user_credentials["user_uid"],
@@ -275,7 +571,7 @@ class TestSurveys:
                     "last_updated_at": "2024-01-25 12:38:41.277939",
                     "planned_end_date": "2021-12-31",
                     "planned_start_date": "2021-01-01",
-                    "prime_geo_level_uid": None,
+                    "prime_geo_level_uid": 1,
                     "project_name": "Test Project",
                     "state": "Draft",
                     "survey_description": "A survey admin survey",
@@ -342,7 +638,7 @@ class TestSurveys:
                     "planned_start_date": "2021-01-01",
                     "planned_end_date": "2021-12-31",
                     "state": "Draft",
-                    "prime_geo_level_uid": None,
+                    "prime_geo_level_uid": 1,
                     "config_status": "In Progress - Configuration",
                     "last_updated_at": "2023-05-30 00:00:00",
                     "created_by_user_uid": test_user_credentials["user_uid"],
@@ -441,7 +737,7 @@ class TestSurveys:
                     "planned_start_date": "2021-01-01",
                     "planned_end_date": "2021-12-31",
                     "state": "Draft",
-                    "prime_geo_level_uid": None,
+                    "prime_geo_level_uid": 1,
                     "config_status": "In Progress - Configuration",
                     "last_updated_at": "2023-05-30 00:00:00",
                     "created_by_user_uid": test_user_credentials["user_uid"],
@@ -631,7 +927,15 @@ class TestSurveys:
         login_user(client, test_user_credentials)
 
     def test_get_config_status(
-        self, client, login_test_user, csrf_token, create_surveys, create_parent_form
+        self,
+        client,
+        login_test_user,
+        csrf_token,
+        create_surveys,
+        create_parent_form,
+        create_roles,
+        upload_enumerators_csv,
+        upload_targets_csv,
     ):
         """
         Test that module config status for the survey can be retrieved
@@ -647,16 +951,16 @@ class TestSurveys:
                 "Module selection": {"status": "Not Started"},
                 "Survey information": [
                     {"name": "SurveyCTO information", "status": "In Progress"},
-                    {"name": "Field supervisor roles", "status": "Not Started"},
-                    {"name": "Survey locations", "status": "Not Started"},
+                    {"name": "User and role management", "status": "In Progress"},
+                    {"name": "Survey locations", "status": "In Progress"},
                     {"name": "SurveyStream users", "status": "Not Started"},
-                    {"name": "Enumerators", "status": "Not Started"},
-                    {"name": "Targets", "status": "Not Started"},
+                    {"name": "Enumerators", "status": "In Progress"},
+                    {"name": "Targets", "status": "In Progress"},
                 ],
                 "overall_status": "In Progress - Configuration",
             },
             "success": True,
-            "success": True,
         }
+
         checkdiff = jsondiff.diff(expected_response, response.json)
         assert checkdiff == {}
