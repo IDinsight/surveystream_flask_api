d81634af203ed7f4b1e26e680fc8d8f248c5fee9
jeenut27 <jeenuthomas27@gmail.com>
2024-07-21 20:10:46 +0530

    adding custom logic for column config module

--- app/blueprints/module_selection/controllers.py
+++ app/blueprints/module_selection/controllers.py
@@ -12,7 +12,19 @@ from .validators import UpdateModuleStatusValidator, AddModuleStatusValidator
 @module_selection_bp.route("/modules", methods=["GET"])
 @logged_in_active_user_required
 def list_modules():
-    modules = Module.query.all()
+    """
+    Function to list all the modules available in the system
+    """
+    modules = (
+        db.session.query(Module)
+        .filter(
+            Module.module_id.notin_(
+                [16]
+            )  # Excluding the module with id 16: Assignments column configuration
+        )
+        .all()
+    )
+
     if not modules:
         return jsonify({"success": False, "message": "No modules found."}), 404
     return (
@@ -26,8 +38,18 @@ def list_modules():
 @validate_payload(AddModuleStatusValidator)
 @custom_permissions_required("ADMIN", "body", "survey_uid")
 def add_module_status(validated_payload):
+    """
+    Function to add the module selection results to module status table for a survey
+
+    """
     survey_uid = validated_payload.survey_uid.data
     modules = validated_payload.modules.data
+
+    # If '9: Assignments' is selected, automatically select '16: Assignments column configuration'
+    if "9" in modules:
+        if "16" not in modules:
+            modules.append("16")
+
     existing_modules_status = ModuleStatus.query.filter_by(survey_uid=survey_uid).all()
     deselected_modules_status = filter(
         lambda module: str(module.module_id) not in modules, existing_modules_status
@@ -82,7 +104,19 @@ def add_module_status(validated_payload):
 @module_selection_bp.route("/module-status/<int:survey_uid>", methods=["GET"])
 @logged_in_active_user_required
 def get_module_status(survey_uid):
-    module_status = ModuleStatus.query.filter_by(survey_uid=survey_uid).all()
+    """
+    Get the status of all the modules for a survey
+    """
+    module_status = (
+        db.session.query(ModuleStatus)
+        .filter(
+            ModuleStatus.survey_uid == survey_uid,
+            ModuleStatus.module_id.notin_(
+                [16]
+            ),  # Excluding the module with id 16: Assignments column configuration
+        )
+        .all()
+    )
     return (
         jsonify(
             {"success": True, "data": [status.to_dict() for status in module_status]}
