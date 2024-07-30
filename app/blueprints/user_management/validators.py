dc7376f69c1f496c2959edd68e404ae9a284971e
jeenut27 <jeenuthomas27@gmail.com>
2024-07-30 13:32:48 +0530

    adding gender, language and location to user

--- app/blueprints/user_management/validators.py
+++ app/blueprints/user_management/validators.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import (
-    StringField,
-    PasswordField,
-    validators,
     BooleanField,
     FieldList,
     IntegerField,
+    PasswordField,
+    StringField,
+    validators,
 )
 from wtforms.validators import DataRequired, Email, Optional, ValidationError
+
+from app.blueprints.auth.models import User
+from app.blueprints.locations.models import Location
+from app.blueprints.surveys.models import Survey
 
 
 class GetUsersQueryParamValidator(FlaskForm):
@@ -30,11 +34,46 @@ class CheckUserValidator(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
 
 
+def validate_locations(form, field):
+    """
+    Custom validator to validate that the locations provided are prime geo level locations.
+    """
+    # Get the prime geo level from the survey configuration
+    prime_geo_level_uid = (
+        Survey.query.filter_by(survey_uid=form.survey_uid.data)
+        .first()
+        .prime_geo_level_uid
+    )
+    if not prime_geo_level_uid:
+        raise ValidationError(
+            "A prime geo level must be defined for the survey for user location mapping."
+        )
+
+    for location_uid in field.data:
+        location = Location.query.get(location_uid)
+        if not location:
+            raise ValidationError(f"Location with UID {location_uid} does not exist.")
+
+        if location.geo_level_uid != prime_geo_level_uid:
+            raise ValidationError(
+                f"Location with UID {location_uid} is not a prime geo level location."
+            )
+
+
 class AddUserValidator(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
     roles = FieldList(StringField("Roles"), default=[], validators=[Optional()])
+
+    gender = StringField("Gender", validators=[Optional()])
+    languages = FieldList(StringField("Language"), default=[], validators=[Optional()])
+    locations = FieldList(
+        IntegerField("Location"),
+        default=[],
+        validators=[Optional(), validate_locations],
+    )
+
     is_super_admin = BooleanField(
         "is_super_admin", default=False, validators=[Optional()]
     )
@@ -57,6 +96,13 @@ class EditUserValidator(FlaskForm):
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
     roles = FieldList(StringField("Roles"), default=[], validators=[Optional()])
+    gender = StringField("Gender", validators=[Optional()])
+    languages = FieldList(StringField("Language"), default=[], validators=[Optional()])
+    locations = FieldList(
+        IntegerField("Location"),
+        default=[],
+        validators=[Optional(), validate_locations],
+    )
     is_super_admin = BooleanField(
         "Is Super Admin", default=False, validators=[Optional()]
     )
@@ -87,3 +133,29 @@ class CompleteRegistrationValidator(FlaskForm):
             validators.EqualTo("new_password", message="Passwords must match"),
         ],
     )
+
+
+class UserLocationsParamValidator(FlaskForm):
+    class Meta:
+        csrf = False
+
+    survey_uid = IntegerField(validators=[DataRequired()])
+    user_uid = IntegerField(validators=[DataRequired()])
+
+
+class UserLocationsPayloadValidator(FlaskForm):
+    survey_uid = IntegerField(validators=[DataRequired()])
+    user_uid = IntegerField(validators=[DataRequired()])
+    locations = FieldList(
+        IntegerField(), validators=[DataRequired(), validate_locations]
+    )
+
+    def validate_survey_uid(form, field):
+        survey = Survey.query.get(field.data)
+        if not survey:
+            raise ValidationError(f"Survey with UID {field.data} does not exist.")
+
+    def validate_user_uid(form, field):
+        user = User.query.get(field.data)
+        if not user:
+            raise ValidationError(f"User with UID {field.data} does not exist.")
