05289afe39690843391c84779cf6e7b4650e9ec1
WanjohiWanjohi <18749225+WanjohiWanjohi@users.noreply.github.com>
2025-01-29 14:06:34 +0300

    Update endpoint to also accept the number of attempts

--- app/blueprints/forms/controllers.py
+++ app/blueprints/forms/controllers.py
@@ -138,6 +138,11 @@ def create_form(validated_payload):
             jsonify({"error": "form_type=admin must have a admin_form_type defined"}),
             422,
         )
+    if (validated_payload.form_type.data == "parent") and validate_payload.number_of_attempts.data is None:
+        return (
+            jsonify({"error": "form_type=parent must have a number_of_attempts defined"}),
+            422,
+        )
 
     form = Form(
         survey_uid=validated_payload.survey_uid.data,
@@ -152,10 +157,8 @@ def create_form(validated_payload):
         dq_form_type=validated_payload.dq_form_type.data,
         admin_form_type=validated_payload.admin_form_type.data,
         parent_form_uid=validated_payload.parent_form_uid.data,
-        number_of_attempts = None
+        number_of_attempts = None  or validated_payload.number_of_attempts.data,
     )
-    if form.form_type =='parent':
-        form.number_of_attempts = validated_payload.number_of_attempts.data
     try:
         db.session.add(form)
         db.session.commit()
@@ -216,6 +219,11 @@ def update_form(form_uid, validated_payload):
             jsonify({"error": "form_type=admin must have a admin_form_type defined"}),
             422,
         )
+    if (validated_payload.form_type.data == "parent") and validate_payload.number_of_attempts.data is None:
+        return (
+            jsonify({"error": "form_type=parent must have a number_of_attempts defined"}),
+            422
+        )
 
     try:
         Form.query.filter_by(form_uid=form_uid).update(
@@ -231,6 +239,7 @@ def update_form(form_uid, validated_payload):
                 Form.dq_form_type: validated_payload.dq_form_type.data,
                 Form.admin_form_type: validated_payload.admin_form_type.data,
                 Form.parent_form_uid: validated_payload.parent_form_uid.data,
+                Form.number_of_attempts: None or validated_payload.number_of_attempts.data,
             },
             synchronize_session="fetch",
         )
