97ac5f1a3f0627bcdec00f95a4a52aca1969e21e
WanjohiWanjohi <18749225+WanjohiWanjohi@users.noreply.github.com>
2025-02-03 11:07:17 +0300

    Update test_update_target_scto_columns_exception_no_scto_server_present tes payload

--- tests/unit/test_targets.py
+++ tests/unit/test_targets.py
@@ -1665,6 +1665,7 @@ class TestTargets:
             "form_type": "parent",
             "parent_form_uid": None,
             "dq_form_type": None,
+            "number_of_attempts": 7,
         }
 
         response = client.put(
