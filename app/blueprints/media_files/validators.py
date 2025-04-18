1caa76aa6eeceaafa9437da237cfed0039c9d15d
utkarsh14 <22423627+utkarsh14@users.noreply.github.com>
2025-04-18 10:52:59 +0530

    Media files: Add format and media_fields columns

--- app/blueprints/media_files/validators.py
+++ app/blueprints/media_files/validators.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, FieldList, StringField
+from wtforms import FieldList, IntegerField, StringField
 from wtforms.validators import DataRequired
 
 
@@ -14,12 +14,32 @@ class CreateMediaFilesConfigValidator(FlaskForm):
     form_uid = IntegerField(validators=[DataRequired()])
     file_type = StringField(validators=[DataRequired()])
     source = StringField(validators=[DataRequired()])
+    format = StringField(validators=[DataRequired()])
     scto_fields = FieldList(StringField(), validators=[DataRequired()])
+    media_fields = FieldList(
+        StringField(),
+        default=[],
+        validators=[
+            lambda form, field: (
+                DataRequired()(form, field) if form.format.data == "wide" else None
+            )
+        ],
+    )
     mapping_criteria = StringField()
 
 
 class MediaFilesConfigValidator(FlaskForm):
     file_type = StringField(validators=[DataRequired()])
     source = StringField(validators=[DataRequired()])
+    format = StringField(validators=[DataRequired()])
     scto_fields = FieldList(StringField(), validators=[DataRequired()])
+    media_fields = FieldList(
+        StringField(),
+        default=[],
+        validators=[
+            lambda form, field: (
+                DataRequired()(form, field) if form.format.data == "wide" else None
+            )
+        ],
+    )
     mapping_criteria = StringField()
