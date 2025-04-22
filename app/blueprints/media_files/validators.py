5d810aac8297084b916aa359c9d6be8260c57115
utkarsh14 <22423627+utkarsh14@users.noreply.github.com>
2025-04-22 12:15:11 +0530

    [SS-2063] Media files: Add format and media_fields columns (#304)
    * Media files: Add format and media_fields columns
    
    * Set format only for surveycto source

--- app/blueprints/media_files/validators.py
+++ app/blueprints/media_files/validators.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, FieldList, StringField
+from wtforms import FieldList, IntegerField, StringField
 from wtforms.validators import DataRequired
 
 
@@ -14,12 +14,45 @@ class CreateMediaFilesConfigValidator(FlaskForm):
     form_uid = IntegerField(validators=[DataRequired()])
     file_type = StringField(validators=[DataRequired()])
     source = StringField(validators=[DataRequired()])
+    format = StringField(
+        validators=[
+            lambda form, field: (
+                DataRequired()(form, field) if form.source.data == "SurveyCTO" else None
+            )
+        ],
+    )
     scto_fields = FieldList(StringField(), validators=[DataRequired()])
+    media_fields = FieldList(
+        StringField(),
+        default=[],
+        validators=[
+            lambda form, field: (
+                DataRequired()(form, field) if form.format.data == "wide" else None
+            )
+        ],
+    )
     mapping_criteria = StringField()
 
 
 class MediaFilesConfigValidator(FlaskForm):
     file_type = StringField(validators=[DataRequired()])
     source = StringField(validators=[DataRequired()])
+    format = StringField(
+        default="long",
+        validators=[
+            lambda form, field: (
+                DataRequired()(form, field) if form.source.data == "surveycto" else None
+            )
+        ],
+    )
     scto_fields = FieldList(StringField(), validators=[DataRequired()])
+    media_fields = FieldList(
+        StringField(),
+        default=[],
+        validators=[
+            lambda form, field: (
+                DataRequired()(form, field) if form.format.data == "wide" else None
+            )
+        ],
+    )
     mapping_criteria = StringField()
